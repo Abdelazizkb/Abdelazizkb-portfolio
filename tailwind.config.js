06a43fdc37372c3dd84b55aa9b5d3a578c4d9153
Abdelazizkb <61007391+Abdelazizkb@users.noreply.github.com>
2025-01-25 01:44:22 +0100

    Features : hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#357EF5",
+        "purple-primary": "#A855F7",
+        secondary: "#121314",
+        "secondary-light": "#5E6971",
+      },
+      container: {
+        xs: "410px",
+      },
+    },
   },
   plugins: [],
-}
+};
